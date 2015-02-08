f0d260a70b9a9376947b0f3eec2a145a70701484
Urpokerho local <urpokerho@urpobotti.local>
2015-02-08 14:19:07 +0200

    reminder that we use upstart now

--- python/start_services.sh
+++ python/start_services.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "This is for tesing only, remember to stop the services managed by upstart before running this script"
 source ~/.virtualenvs/urpobotti/bin/activate
 ./motorctrl.py /dev/ttyO3 &
 ./pinger.py /dev/ttyUSB0 &
