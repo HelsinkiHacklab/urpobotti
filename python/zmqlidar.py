e14678ff7fab910d4f500edc778083c90f5f290e
Eero af Heurlin <eero.afheurlin@iki.fi>
2016-06-10 23:27:39 +0300

    implement BreezySLAM compatible wrapper for ZMQ provided data

---
+++ python/zmqlidar.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import zmq
+from zmq.eventloop import ioloop as ioloop_mod
+import zmqdecorators
+import json
+import time
+
+class ZMQLidar(zmqdecorators.client):
+    lidar_data = [(0,0) for x in range(360)]
+    _last_lidar_time = None
+    speed_rpm = 300
+
+    def __init__(self, service_name):
+        super(ZMQLidar, self).__init__()
+        zmqdecorators.subscribe_topic(service_name, 'lidar', self.lidar_callback)
+        # TODO: check how old our data is and null the array if too old
+        zmqdecorators.subscribe_topic(service_name, 'lidar_rpm', self.rpm_callback)
+
+    def lidar_callback(self, jsondata, timestamp):
+        # TODO:reject too old data ?
+        self.lidar_data = json.loads(jsondata)
+        self._last_lidar_time = time.time()
+
+    def rpm_callback(self, rpm, timestamp):
+        # TODO:reject too old data ?
+        self.speed_rpm = float(rpm)
+
+    def getScan(self):
+        return self.lidar_data
+
+    def getRPM(self):
+        return self.speed_rpm
+
